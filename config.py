203523b6b2da23192420fa6b1ca726e456494b5b
Jdmast <103869942+Jdmast@users.noreply.github.com>
2022-04-17 10:41:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID ="16432997" int(os.getenv("API_ID"))
+API_HASH ="0840e001fad4699da3d7d87e2a4066aa" os.getenv("API_HASH")
+BOT_TOKEN ="5396516569:AAFSHbNMhAQv25K4fffVCz-LfPP_vZEaD-U" os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
